5816c838929300812878197f7e82dc5d4d37a969
耿一斐 <gengyf@dxy.cn>
2018-08-23 18:25:58 +0800

    [add] 去除html标签

--- common-utils/src/main/java/org/pigeon/string/StringUtils.java
+++ common-utils/src/main/java/org/pigeon/string/StringUtils.java
@@ -1,5 +1,6 @@
 package org.pigeon.string;
 
+import com.google.common.base.Strings;
 import org.apache.log4j.Logger;
 
 import java.net.URLDecoder;
@@ -18,6 +19,93 @@ public class StringUtils {
     private static final String GBK = "gbk";
 
 
+    private static int[] filter = new int[128];
+    private static int[] filterEnd = new int[128];
+
+    static {
+        filter['<'] = Integer.MAX_VALUE / 2;
+        filterEnd['<'] = '>';
+
+        filter['&'] = 10;
+        filterEnd['&'] = ';';
+
+        filter[';'] = -1;
+        filter['\n'] = -1;
+
+        filter['\r'] = -1;
+        filter['\t'] = -1;
+        filter[' '] = 1;
+        filter['*'] = 1;
+        filter['-'] = 1;
+//        filter['.'] = 1;
+        filter['#'] = 1;
+
+    }
+
+    /**
+     * 去除html标签
+     *
+     * @param input
+     * @return
+     */
+    public static String rmHtmlTag(String input) {
+
+        if (Strings.isNullOrEmpty(input)) {
+            return NULL_STRING;
+        }
+        int length = input.length();
+        int tl;
+        StringBuilder sb = new StringBuilder();
+        char c;
+        for (int i = 0; i < length; i++) {
+            c = input.charAt(i);
+
+            if (c > 127) {
+                sb.append(c);
+                continue;
+            }
+
+            switch (filter[c]) {
+                case -1:
+                    sb.append(" ");
+                    break;
+                case 0:
+                    sb.append(c);
+                    break;
+                case 1:
+                    if (sb.length() > 0 && sb.charAt(sb.length() - 1) != c)
+                        sb.append(c);
+                    do {
+                        i++;
+                    } while (i < length && input.charAt(i) == c);
+
+                    if (i < length || input.charAt(length - 1) != c)
+                        i--;
+                    break;
+                default:
+                    tl = filter[c] + i;
+                    int tempOff = i;
+                    boolean flag = false;
+                    char end = (char) filterEnd[c];
+                    for (i++; i < length && i < tl; i++) {
+                        c = input.charAt(i);
+                        if (c > 127) continue;
+                        if (c == end) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) {
+                        i = tempOff;
+                        sb.append(input.charAt(i));
+                    }else {
+                        sb.append(" ");
+                    }
+                    break;
+            }
+        }
+        return sb.toString();
+    }
 
     /**
      * toString,为空时返回""
